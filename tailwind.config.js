b7b5f9f132cb075cb6eed1347e51f0ff89bd7f28
LeGion <mehrdad.dadashi@gmail.com>
2025-01-14 17:49:32 +0300

    chore: update tailwind.config.js with standardized colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,27 @@ module.exports = {
   content: ["./pages/*.html"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6c7fd8",
+        dark: "#3d4750",
+        medium: "#686e7d",
+        light: "#777777",
+        box: "#f8f8fb",
+        accent: "#fea99a",
+      },
+      fontSize: {
+        13: "0.8125rem",
+        15: "0.9375rem",
+        40: "2.5rem",
+        50: "3.125rem",
+      },
+      fontFamily:{
+        quicksand:"quicksand",
+        poppins:["Poppins, sans-serif"],
+
+      }
+    },
   },
   plugins: [],
-}
-
+};
