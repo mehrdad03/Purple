88fd7adc07845286c41061d7181e111f862dcde7
LeGion <mehrdad.dadashi@gmail.com>
2025-01-25 17:09:10 +0300

    Hero and Explore categories sections is done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,14 +17,23 @@ module.exports = {
           xl: "1320px",
         },
       },
+      width: {
+        30: "1.875rem",
+      },
+      height: {
+        30: "1.875rem",
+        60: "3.75rem",
+      },
       borderColor: {
         "light-gray": "#EEEEEE",
       },
       borderRadius: {
         10: "0.625rem",
         20: "1.25rem",
+        30: "1.875rem",
       },
       spacing: {
+        30: "1.875rem",
         50: "3.125rem",
       },
       colors: {
@@ -34,12 +43,18 @@ module.exports = {
         medium: "#686E7D",
         box: "#F8F8FB",
         accent: "#FEA99A",
+        "cat-box-color-1": "#FEF1F1",
+        "cat-box-color-2": "#FBF9E4",
+        "cat-box-color-3": "#F4F1FE",
+        "cat-box-color-4": "#E1FCF2",
       },
       fontSize: {
         13: "0.8125rem",
         14: "0.875rem",
         15: "0.9375rem",
         31: "1.9375rem",
+        50: "3.125rem",
+        124: "7.75rem",
       },
       boxShadow: {
         submenu: "2px 2px 15px 5px rgba(0, 0, 0, 0.07)",
